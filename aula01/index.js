78c3ca5771b01e5efc763d9dfdd6a48ded2e2b2d
Michael Santos <mackelldev@gmail.com>
2020-06-25 06:12:30 -0300

    arquivos primeiras aulas

---
+++ aula01/index.js
@@ -0,0 +1,5 @@
+//Primeiro projeto usando NodeJS
+console.log('Olá, Mundo!')
+const a = 3
+const b = 2
+console.log(a + b)
---
+++ aula02/index.js
@@ -0,0 +1,5 @@
+/**
+ * 0 - Obter endereço usuário
+ * 1 - Obter o número de telefone do usuario pelo Id
+ * 2 - Obter o endereço do usuário pelo Id
+ */
